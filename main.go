7f0ed5289de12398684ad62285641385c6784ba1
Zeu Cxb <zeucxb@gmail.com>
2017-05-06 23:56:51 -0300

    Create a package parser with a parse function

--- main.go
+++ main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
 	"os"
-	"regexp"
-	"strconv"
-	"strings"
-
-	"encoding/json"
-
-	"io/ioutil"
+	"quake-log-parser/parser"
 
 	cli "gopkg.in/urfave/cli.v2"
 )
@@ -40,97 +33,7 @@ func main() {
 				Usage:       "run the parser",
 				Description: "This run a parser",
 				Action: func(c *cli.Context) (err error) {
-					killRegEx, err := regexp.Compile(`Kill:\s(.*?):`)
-					if err != nil {
-						return
-					}
-
-					userRegEx, err := regexp.Compile(`ClientUserinfoChanged:\s(.)\sn\\(.*)\\t\\`)
-					if err != nil {
-						return
-					}
-
-					gameStartRegEx, err := regexp.Compile(`InitGame:`)
-					if err != nil {
-						return
-					}
-
-					fileStr := c.Args().Get(0)
-					if fileStr == "" {
-						fileStr = "games.log"
-					}
-
-					file, err := os.Open(fileStr)
-					if err != nil {
-						return
-					}
-
-					games := make(map[string]*game)
-					count := 1
-					keyPref := "game_"
-
-					var key string
-
-					scanner := bufio.NewScanner(file)
-					for scanner.Scan() {
-						if match := killRegEx.FindStringSubmatch(scanner.Text()); len(match) == 2 {
-							killInfo := strings.Split(match[1], " ")
-
-							killerKey, err := strconv.Atoi(killInfo[0])
-							if err != nil {
-								panic(err)
-							}
-
-							killedKey, err := strconv.Atoi(killInfo[1])
-							if err != nil {
-								panic(err)
-							}
-
-							if killInfo[0] == "1022" {
-								games[key].Kills[games[key].Players[killedKey-1]]--
-							} else {
-								games[key].Kills[games[key].Players[killerKey-1]]++
-							}
-
-							games[key].TotalKills++
-
-							continue
-						}
-
-						if match := userRegEx.FindStringSubmatch(scanner.Text()); len(match) == 3 {
-							playerKey, err := strconv.Atoi(match[1])
-							if err != nil {
-								panic(err)
-							}
-
-							games[key].Players[playerKey-1] = match[2]
-
-							continue
-						}
-
-						if match := gameStartRegEx.MatchString(scanner.Text()); match {
-							key = fmt.Sprintf("%s%v", keyPref, count)
-
-							if _, ok := games[key]; !ok {
-								games[key] = &game{
-									Players: make(map[int]string),
-									Kills:   make(map[string]int),
-								}
-							}
-
-							count++
-
-							continue
-						}
-					}
-					if err := scanner.Err(); err != nil {
-						fmt.Fprintln(os.Stderr, "reading standard input:", err)
-					}
-
-					json, err := json.Marshal(games)
-
-					err = ioutil.WriteFile("quake_data.json", json, 0644)
-
+					err = parser.Parse()
 					return
 				},
 			},
---
+++ parser/parser.go
@@ -0,0 +1,107 @@
+package parser
+
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Parse() (err error) {
+	killRegEx, err := regexp.Compile(`Kill:\s(.*?):`)
+	if err != nil {
+		return
+	}
+
+	userRegEx, err := regexp.Compile(`ClientUserinfoChanged:\s(.)\sn\\(.*)\\t\\`)
+	if err != nil {
+		return
+	}
+
+	gameStartRegEx, err := regexp.Compile(`InitGame:`)
+	if err != nil {
+		return
+	}
+
+	fileStr := c.Args().Get(0)
+	if fileStr == "" {
+		fileStr = "games.log"
+	}
+
+	file, err := os.Open(fileStr)
+	if err != nil {
+		return
+	}
+
+	games := make(map[string]*game)
+	count := 1
+	keyPref := "game_"
+
+	var key string
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		if match := killRegEx.FindStringSubmatch(scanner.Text()); len(match) == 2 {
+			killInfo := strings.Split(match[1], " ")
+
+			killerKey, err := strconv.Atoi(killInfo[0])
+			if err != nil {
+				panic(err)
+			}
+
+			killedKey, err := strconv.Atoi(killInfo[1])
+			if err != nil {
+				panic(err)
+			}
+
+			if killInfo[0] == "1022" {
+				games[key].Kills[games[key].Players[killedKey-1]]--
+			} else {
+				games[key].Kills[games[key].Players[killerKey-1]]++
+			}
+
+			games[key].TotalKills++
+
+			continue
+		}
+
+		if match := userRegEx.FindStringSubmatch(scanner.Text()); len(match) == 3 {
+			playerKey, err := strconv.Atoi(match[1])
+			if err != nil {
+				panic(err)
+			}
+
+			games[key].Players[playerKey-1] = match[2]
+
+			continue
+		}
+
+		if match := gameStartRegEx.MatchString(scanner.Text()); match {
+			key = fmt.Sprintf("%s%v", keyPref, count)
+
+			if _, ok := games[key]; !ok {
+				games[key] = &game{
+					Players: make(map[int]string),
+					Kills:   make(map[string]int),
+				}
+			}
+
+			count++
+
+			continue
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+	}
+
+	json, err := json.Marshal(games)
+
+	err = ioutil.WriteFile("quake_data.json", json, 0644)
+
+	return
+}
