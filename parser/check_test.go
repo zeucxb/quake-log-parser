0a9a17cedb53b5c50aa6b62972c966d663ddea04
Zeu Cxb <zeucxb@gmail.com>
2017-05-08 23:32:48 -0300

    Add tests to the USER parse exeptions, make TDD correct

--- parser/check_test.go
+++ parser/check_test.go
@@ -20,6 +20,7 @@ func Test(t *testing.T) {
 
 	gameInitText := `0:00 InitGame: \sv_floodProtect\1\sv_maxPing\0\sv_minPing\0\sv_maxRate\10000\sv_minRate\0\sv_hostname\Code Miner Server\g_gametype\0\sv_privateClients\2\sv_maxclients\16\sv_allowDownload\0\dmflags\0\fraglimit\20\timelimit\15\g_maxGameClients\0\capturelimit\8\version\ioq3 1.36 linux-x86_64 Apr 12 2009\protocol\68\mapname\q3dm17\gamename\baseq3\g_needpass\0`
 	userText := `0:10 ClientUserinfoChanged: 2 n\Isgalamido\t\0\model\xian/default\hmodel\xian/default\g_redteam\\g_blueteam\\c1\4\c2\5\hc\100\w\0\l\0\tt\0\tl\0`
+	userUpdateText := `0:15 ClientUserinfoChanged: 2 n\Isgalamido2\t\0\model\xian/default\hmodel\xian/default\g_redteam\\g_blueteam\\c1\4\c2\5\hc\100\w\0\l\0\tt\0\tl\0`
 	worldKillText := `0:17 Kill: 1022 2 22: <world> Isgalamido by MOD_TRIGGER_HURT`
 
 	userText2 := `0:20 ClientUserinfoChanged: 3 n\Dono da Bola\t\0\model\xian/default\hmodel\xian/default\g_redteam\\g_blueteam\\c1\4\c2\5\hc\100\w\0\l\0\tt\0\tl\0`
@@ -95,6 +96,20 @@ func Test(t *testing.T) {
 			game := games["game_1"]
 
 			g.Assert(game.Players).Equal(map[int]string{1: "Isgalamido"})
+			g.Assert(game.Kills).Equal(map[string]int{"Isgalamido": 0})
+		})
+
+		g.It("Should update a player correctly", func() {
+			checkAndParseUser(userText)
+
+			checkAndParseKill(playerKillText)
+
+			checkAndParseUser(userUpdateText)
+
+			game := games["game_1"]
+
+			g.Assert(game.Players).Equal(map[int]string{1: "Isgalamido2"})
+			g.Assert(game.Kills).Equal(map[string]int{"Isgalamido2": 1})
 		})
 	})
 
